33a3b47901df1e1b6d56a7a369a432a07e20b765
liuchun <robin_liu1989@sina.com>
2017-09-17 14:14:38 +0800

    添加文件

---
+++ myapp/src/pages/User.js
@@ -0,0 +1,19 @@
+/**
+ * Created by LisaTsang on 2017/9/17.
+ */
+
+import React, { Component } from 'react';
+
+export  default class User extends Component {
+    render() {
+        return (
+            <div >
+                <div>{this.props.title}</div>
+
+            </div>
+        );
+    }
+}
+
+
+
